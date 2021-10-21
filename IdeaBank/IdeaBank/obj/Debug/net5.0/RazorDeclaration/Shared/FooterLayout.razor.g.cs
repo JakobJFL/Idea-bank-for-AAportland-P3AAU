735f860f8fa1ff3ffffeec17f64e651ffab9a2f9
Jakob Lykke <47578462+JakobJFL@users.noreply.github.com>
2021-10-21 11:44:53 +0200

    colours fixed added to C#

--- IdeaBank/IdeaBank/obj/Debug/net5.0/RazorDeclaration/Shared/FooterLayout.razor.g.cs
+++ IdeaBank/IdeaBank/obj/Debug/net5.0/RazorDeclaration/Shared/FooterLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace IdeaBank.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\JFL\Documents\GitHub\Idea-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
+#line 1 "C:\Users\Jakob\Documents\GitHub\Ideer-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\JFL\Documents\GitHub\Idea-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
+#line 2 "C:\Users\Jakob\Documents\GitHub\Ideer-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\JFL\Documents\GitHub\Idea-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
+#line 3 "C:\Users\Jakob\Documents\GitHub\Ideer-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\JFL\Documents\GitHub\Idea-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
+#line 4 "C:\Users\Jakob\Documents\GitHub\Ideer-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\JFL\Documents\GitHub\Idea-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
+#line 5 "C:\Users\Jakob\Documents\GitHub\Ideer-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\JFL\Documents\GitHub\Idea-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
+#line 6 "C:\Users\Jakob\Documents\GitHub\Ideer-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\JFL\Documents\GitHub\Idea-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
+#line 7 "C:\Users\Jakob\Documents\GitHub\Ideer-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\JFL\Documents\GitHub\Idea-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
+#line 8 "C:\Users\Jakob\Documents\GitHub\Ideer-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\JFL\Documents\GitHub\Idea-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
+#line 9 "C:\Users\Jakob\Documents\GitHub\Ideer-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
 using IdeaBank;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\JFL\Documents\GitHub\Idea-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
+#line 10 "C:\Users\Jakob\Documents\GitHub\Ideer-bank-for-AAportland-P3AAU\IdeaBank\IdeaBank\_Imports.razor"
 using IdeaBank.Shared;
 
 #line default
