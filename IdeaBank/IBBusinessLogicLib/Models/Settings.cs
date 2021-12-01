031b0c3287e3f1188f7068f0c4e1a6ac1efd8921
APaludan <andreashp@outlook.com>
2021-12-01 10:41:57 +0100

    comments enabled by default

--- IdeaBank/IBBusinessLogicLib/Models/Settings.cs
+++ IdeaBank/IBBusinessLogicLib/Models/Settings.cs
@@ -2,7 +2,7 @@ namespace BusinessLogicLib.Models
 {
     public class Settings
     {
-        public bool CommentsEnabled { get; set; }
+        public bool CommentsEnabled { get; set; } = true;
         public string Purpose { get; set; }
         public string HomepageGuide { get; set; }
         public string SubmitGuide { get; set; }
