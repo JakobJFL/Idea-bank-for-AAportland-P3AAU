85e3cd8ecb9bdd33a68f246d14bf25b06871993d
Jakob Lykke <47578462+JakobJFL@users.noreply.github.com>
2022-01-20 14:02:48 +0100

    Fixed CommentsTbl Message Migration

--- IdeaBank/IBDataAccess/Migrations/20220120111435_CommentsTblUpdate.Designer.cs
+++ IdeaBank/IBDataAccess/Migrations/20220120111435_CommentsTblUpdate.Designer.cs
@@ -4,14 +4,16 @@ using DataBaseLib.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataBaseLib.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220120111435_CommentsTblUpdate")]
+    partial class CommentsTblUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IdeaBank/IBDataAccess/Migrations/20220120111435_CommentsTblUpdate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataBaseLib.Migrations
+{
+    public partial class CommentsTblUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "CommentsTbl",
+                type: "nvarchar(1500)",
+                maxLength: 1500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(1500)",
+                oldMaxLength: 1500);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "CommentsTbl",
+                type: "varchar(1500)",
+                maxLength: 1500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1500)",
+                oldMaxLength: 1500);
+        }
+    }
+}
